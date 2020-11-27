eb29dcab0bc75fd20de4af18323e1ebd17c7b7b0
Rodgers Ouma <mc_alila@outlook.com>
2020-11-27 22:08:01 +0300

    Bye bye firestore

--- crawler.go
+++ crawler.go
@@ -1,72 +1,63 @@
 package main
 
 import (
-	"context"
 	"encoding/xml"
 	"io/ioutil"
 	"net/http"
-	"strconv"
 	"sync"
 	"time"
 
-	"cloud.google.com/go/firestore"
 	"github.com/rs/zerolog/log"
-	"google.golang.org/api/iterator"
 )
 
-func fetchUsers(ctx context.Context, fire *firestore.Client) {
-	iter := fire.Collection("userFeeds").Documents(ctx)
+// RawFeed is a raw feed model
+type RawFeed struct {
+	URL   string `json:"url" firestore:"url"`
+	IsRSS bool   `json:"isRSS" firestore:"isRSS"`
+	Name  string `json:"name" firestore:"name"`
+}
+
+func processUsers() {
+	users, err := getUsers()
+	if err != nil {
+		log.Error().Str("error", err.Error()).Msg("Error fetching users")
+		return
+	}
 	var wg sync.WaitGroup
-	for {
-		doc, err := iter.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			log.Error().Str("error", err.Error()).Msg("Error reading crawl Feeds")
-		}
-		user := FireUser{}
-		if err := doc.DataTo(&user); err != nil {
-			log.Error().Str("error", err.Error()).Msg("Error reading firestore users list")
-		}
+	for _, user := range users {
 		lastFetch := time.Now().Add(time.Minute * -30)
-		chatID, _ := strconv.ParseInt(user.ChatID, 0, 64)
 		for _, f := range user.Feeds {
 			wg.Add(1)
-			go fetchFeed(f, true, lastFetch, int(chatID), &wg)
-		}
-		for _, f := range user.Atoms {
-			wg.Add(1)
-			go fetchFeed(f, false, lastFetch, int(chatID), &wg)
+			go fetchFeed(f, lastFetch, user.ID, &wg)
 		}
 	}
 	wg.Wait()
 }
 
-func fetchFeed(str string, isRSS bool, lastUpdated time.Time, chatID int, wg *sync.WaitGroup) {
-	resp, err := http.Get(str)
+func fetchFeed(feed *PgFeed, lastUpdated time.Time, chatID int, wg *sync.WaitGroup) {
+	resp, err := http.Get(feed.Link)
 	if err != nil {
-		log.Error().Str("error", err.Error()).Str("feedUrl", str).Msg("Error fetching feed")
+		log.Error().Str("error", err.Error()).Str("feedURL", feed.Link).Msg("Error fetching feed")
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Error().Str("error", err.Error()).Str("feedUrl", str).Msg("Error processing feed URL")
+		log.Error().Str("error", err.Error()).Str("feedURL", feed.Link).Msg("Error processing feed URL")
 	}
-	if isRSS {
-		feed := Rss{}
-		err = xml.Unmarshal(body, &feed)
-		if err != nil || feed.Channel.Title == "" {
-			log.Error().Str("feedUrl", str).Msg("Not a valid RSS feed:")
+	if feed.IsRSS {
+		fd := Rss{}
+		err = xml.Unmarshal(body, &fd)
+		if err != nil || fd.Channel.Title == "" {
+			log.Error().Str("feedURL", feed.Link).Msg("Not a valid RSS feed:")
 		}
-		feed.toTelegram(lastUpdated, chatID)
+		fd.toTelegram(lastUpdated, chatID)
 	} else {
-		feed := Atom{}
-		err = xml.Unmarshal(body, &feed)
-		if err != nil || feed.Title == "" {
-			log.Error().Str("feedUrl", str).Msg("Not a valid ATOM feed")
+		fd := Atom{}
+		err = xml.Unmarshal(body, &fd)
+		if err != nil || fd.Title == "" {
+			log.Error().Str("feedUrl", feed.Link).Msg("Not a valid ATOM feed")
 		}
-		feed.toTelegram(lastUpdated, chatID)
+		fd.toTelegram(lastUpdated, chatID)
 	}
 	wg.Done()
 }
